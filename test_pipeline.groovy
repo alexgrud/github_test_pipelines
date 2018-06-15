54009f2b1718464db25ea1b965e2f70759091d73
Oleksii Grudev <ogrudev@mirantis.com>
2018-06-15 10:44:08 +0300

    add SALT_MASTER_HOST var

--- test_pipeline.groovy
+++ test_pipeline.groovy
@@ -1,3 +1,5 @@
+// Expected parameters:
+//   SALT_MASTER_HOST
 common = new com.mirantis.mk.Common()
 python = new com.mirantis.mk.Python()
 orchestrate = new com.mirantis.mk.Orchestrate()
@@ -6,23 +8,23 @@ def venv
 def venvPepper
 def outputs = [:]
 node{
-	stage ("Preparing data") {
-	def workspace = common.getWorkspace()
+    stage ("Preparing data") {
+        def workspace = common.getWorkspace()
         venv = "${workspace}/venv"
         venvPepper = "${workspace}/venvPepper"
-	println("Test message")
-	println(venvPepper)
-	saltMasterHost="172.17.49.168"
-	SALT_MASTER_URL = "http://${saltMasterHost}:6969"
-	println(SALT_MASTER_URL)
-	SALT_MASTER_CREDENTIALS="salt-qa-credentials"
-	println(SALT_MASTER_CREDENTIALS)
-	}
-	stage ("installing virtualenv Pepper") {
-		// Setup virtualenv for pepper
-        	python.setupPepperVirtualenv(venvPepper, SALT_MASTER_URL, SALT_MASTER_CREDENTIALS)
-		}
-	stage ("Running salt state") {
-		orchestrate.RunTestSaltCmd(venvPepper)
-		}
+        println("Test message")
+        println(venvPepper)
+        saltMasterHost=SALT_MASTER_HOST
+        SALT_MASTER_URL = "http://${saltMasterHost}:6969"
+        println(SALT_MASTER_URL)
+        SALT_MASTER_CREDENTIALS="salt-qa-credentials"
+        println(SALT_MASTER_CREDENTIALS)
+        }
+        stage ("installing virtualenv Pepper") {
+          // Setup virtualenv for pepper
+          python.setupPepperVirtualenv(venvPepper, SALT_MASTER_URL, SALT_MASTER_CREDENTIALS)
+          }
+        stage ("Running salt state") {
+          orchestrate.RunTestSaltCmd(venvPepper)
+          }
 }

85314e226f6536996a208205b4f884a9273a374c
Oleksii Grudev <ogrudev@mirantis.com>
2018-07-25 15:21:57 +0300

    sync

--- test_pipeline_getconfig.groovy
+++ test_pipeline_getconfig.groovy
@@ -8,21 +8,6 @@ def venv
 def venvPepper
 def outputs = [:]
 def extra_tgt = ""
-def getOrchestrationApplications() {
-  def _orch = salt.getConfig(venvPepper, "I@salt:master ${extra_tgt}", "orchestration.deploy.applications")
-          if ( !_orch['return'][0].values()[0].isEmpty() ) {
-          Map<String,Integer> _orch_app = [:]
-            for (k in _orch['return'][0].values()[0].keySet()) {
-              _orch_app[k] = _orch['return'][0].values()[0][k].values()[0].toInteger()
-            }
-          def _orch_app_sorted = common.SortMapByValueAsc(_orch_app)
-          println(_orch_app_sorted.keySet())
-          def out = orchestrate.OrchestrateApplications(venvPepper, "I@salt:master ${extra_tgt}", _orch_app_sorted.keySet())
-          }
-          else {
-            common.infoMsg("No applications found for orchestration")
-          }
-}
 node{
     stage ("Preparing data") {
         def workspace = common.getWorkspace()
@@ -35,7 +20,22 @@ node{
         println(SALT_MASTER_URL)
         SALT_MASTER_CREDENTIALS="salt-qa-credentials"
         println(SALT_MASTER_CREDENTIALS)
+        def getOrchestrationApplications() {
+          def _orch = salt.getConfig(venvPepper, "I@salt:master ${extra_tgt}", "orchestration.deploy.applications")
+            if ( !_orch['return'][0].values()[0].isEmpty() ) {
+              Map<String,Integer> _orch_app = [:]
+            for (k in _orch['return'][0].values()[0].keySet()) {
+              _orch_app[k] = _orch['return'][0].values()[0][k].values()[0].toInteger()
+            }
+          def _orch_app_sorted = common.SortMapByValueAsc(_orch_app)
+          println(_orch_app_sorted.keySet())
+          def out = orchestrate.OrchestrateApplications(venvPepper, "I@salt:master ${extra_tgt}", _orch_app_sorted.keySet())
+          }
+          else {
+            common.infoMsg("No applications found for orchestration")
+          }
         }
+    }
         stage ("installing virtualenv Pepper") {
           // Setup virtualenv for pepper
           python.setupPepperVirtualenv(venvPepper, SALT_MASTER_URL, SALT_MASTER_CREDENTIALS)

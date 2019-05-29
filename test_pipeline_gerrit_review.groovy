69d2d365cb39aac4c3bf35e2726255da0d2484fb
Oleksii Grudev <ogrudev@mirantis.com>
2019-05-29 17:28:14 +0300

    Add pipeline for uploading gerrit review

---
+++ test_pipeline_gerrit_review.groovy
@@ -0,0 +1,13 @@
+/**
+Expected parameters:
+CREDENTIALS_ID
+*/
+common = new com.mirantis.mk.Common()
+python = new com.mirantis.mk.Python()
+orchestrate = new com.mirantis.mk.Orchestrate()
+salt = new com.mirantis.mk.Salt()
+node{
+    stage ("Preparing directory") {
+    }
+}
+
